4af77663045854fe668225f3a5299cb5d7cb2c5d
Joseph Mburu <josephmacharia575@gmail.com>
2023-06-24 21:38:11 +0300

    COMPLETE

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,19 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def grades
+    grades = Student.select(:id, :grade)
+    render json: grades
+  end
+
+  def highest_grade
+    highest_grade = Student.order(grade: :desc).limit(1)
+    render json: highest_grade
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 
 end
